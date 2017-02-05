baa361ed838897defa0a948070e705f34c9e64ae
Peter Allen <pallen@kieferconsulting.com>
2017-02-05 13:17:45 -0800

    setting up scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+/*
 var gulp = require('gulp');
 var concatCss = require('gulp-concat-css');
  
@@ -5,4 +6,22 @@ gulp.task('default', function () {
   return gulp.src('css/*.css')
     .pipe(concatCss("solution.css"))
     .pipe(gulp.dest(''));
+});
+*/
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concatCss = require('gulp-concat-css');
+
+gulp.task('solution', function() {
+    gulp.src('sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concatCss("solution.css"))
+        .pipe(gulp.dest(''))
+});
+
+//Watch task
+
+gulp.task('default',function() {
+    gulp.watch('sass/*.scss',['solution']);
 });
