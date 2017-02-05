b63dccd5c4e92dade8edfb5876867a6e72a13140
Peter Allen <pallen@kieferconsulting.com>
2017-02-05 13:24:59 -0800

    added theme setup

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concatCss = require('gulp-concat-css');
 
-gulp.task('solution', function() {
+gulp.task('default', function() {
     gulp.src('sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(concatCss("solution.css"))
@@ -21,7 +21,8 @@ gulp.task('solution', function() {
 });
 
 //Watch task
-
+/*
 gulp.task('default',function() {
     gulp.watch('sass/*.scss',['solution']);
 });
+*/
